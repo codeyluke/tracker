c656c1dc114f2b7f2ff2c84ccc60945cfe575dc0
luke <lukekennedy212@gmail.com>
2019-03-12 12:29:49 +0800

    only the destroy left

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < Clearance::UsersController
+    before_action :require_login
+    
+    def index 
+        @users = User.all
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update 
+        selected_user = User.find(params[:id])
+        selected_user.update(user_params)
+        redirect_to selected_user
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def destroy 
+        byebug
+        destroy_user = User.find(params[:id])
+        destroy_user.destroy
+        redirect_to "/"
+    end
+
+    private 
+    def user_params
+        params[:user].permit(
+            :email,
+            :password, 
+            :first_name,
+            :last_name
+        )
+    end
+end
